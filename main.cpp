bb021df2cb7064431a988932a33d0e18627a3f69
Le Duy Anh <25ad11004@student.hcmute.edu.vn>
2026-08-21 09:43:54 +0700

    Update C++ program

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
-int main()
+#include <iostream>     // Thư viện dùng để nhập/xuất dữ liệu
+int main()      // Hàm main - nơi chương trình C++ bắt đầu chạy
 {
-std::cout << "Hello,C++!" << std::endl;
-return 0;
+    std::cout << "================================/n";
+    std::cout << "      LEARN C++ WITH ME        ";
+    std::cout << "================================/n";
+    std::cout << "Name: Le Duy Anh/n";
+    std::cout << "Course: OOP with c++/n";
+    std::cout << "I'm ready to learn c++!/n";
+    std::cout << "/n";
+    std::cout << "My favorite language: Python and c++/n";
+    std::cout << "My goal: Become a software engineer/n";
+    std::cout << "Never stop dreaming and learning!/n";
+    std::cout << "================================/n";
+    std::cout << "           KEEP CODING!         ";
+    std::cout << "================================/n";
+    return 0;       // Trả về 0 để báo hiệu chương trình chạy thành công
 }
